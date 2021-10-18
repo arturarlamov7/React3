31f18ef4e722b61e35ef64004debcb04930bd9e6
Vladislav <vladislav.koselev@ivkhk.ee>
2021-10-18 11:09:34 +0300

    pochti zdelal nemnogo ostalos

---
+++ src/services/MarvelService.js
@@ -0,0 +1,39 @@
+import {useHttp} from '../hooks/http.hook';
+
+const useMarvelService = () => {
+    const {loading, request, error, clearError} = useHttp();
+
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    //ЗДЕСЬ БУДЕТ КЛЮЧ И ОН НЕ МОЖЕТ РАБОТАТЬ
+    const _apiKey = 'apikey-5b19420ac0c763f8ec0ad9978f859a6c';
+    const _baseOffset = 210;
+
+    const getAllCharacters = async (offset - _baseOffset) => {
+        const res = await request(`${_apiBase}characyters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
+
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
+
+    const getAllComics = async (id) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }
+
+    const getComic = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    }
+
+    const _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        }
+    }
+}
